14fee8b7aa17994ec3cb3046558e8b1e00b7d76d
Sam Giagtzoglou <sgiagtzoglou@wesleyan.edu>
2014-09-05 15:51:13 -0400

    backend and db methods

--- backend/db/db.py
+++ backend/db/db.py
@@ -2,3 +2,21 @@ import sqlite3
 
 def connect_database():
 	return sqlite3.connect('db/wesplan.db')
+
+def getMajors(conn):
+	c = conn.cursor()
+	majors = c.execute("select * from majors")
+	majs = []
+	for maj in majors:
+		majs.append(maj[1])
+	return majs
+
+def getMajor(conn, major):
+	c = conn.cursor()
+	major = major.capitalize()
+	r = c.execute("select * from majors where name='{0}'".format(major)).next()
+	return list(r)
+	z = []
+	for e in r:
+		z.append(e)
+	return z
