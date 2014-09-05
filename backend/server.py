e1355a10a4b4a97c82fe556dd974e03f06af29d8
Maxwell Spiegelman <msuser@Maxs-MacBook-Pro-2.local>
2014-09-05 14:25:12 -0400

    backend step functions

--- backend/server.py
+++ backend/server.py
@@ -7,10 +7,23 @@ app = Flask(__name__)
 def main():
 	return "Hello world"
 
+@app.route("/majors")
+def majors():
+	return db.getMajors()
 
+@app.route("/major/<major>")
+def major(major):
+	return db.getMajor(major)
+
+@app.route("/courses/<course>")
+def courses(course):
+	return db.getCourse(course)
 
 
 
 
 if __name__ == "__main__":
     app.run()
+
+
+
