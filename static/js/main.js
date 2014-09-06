70d797de70735b1ab11dc1bcd208a2bde3ca1fee
Maxwell Spiegelman <msuser@Maxs-MacBook-Pro-2.local>
2014-09-06 13:00:40 -0400

    gen ed updated calculator

--- static/js/main.js
+++ static/js/main.js
@@ -11,24 +11,47 @@ function testf(data) {
 function gradProgress(data){
 	courses = data['acadData']['classes']
 	$.getJSON('courses/'+courses.join('_'), function(courseData){
-		credSum = 0
-		nsmSum = 0
-		sbsSum = 0
-		haSum = 0
+		var credSum = 0
 
-		courses = courseData['courses']
+		var nsmSum = 0
+		var nsmDepts = []
+		var sbsSum = 0
+		var sbsDepts = []
+		var haSum = 0
+		var haDepts = []
+
+		var courses = courseData['courses']
 		for (var i = 0; i < courses.length; i++){
 			credSum += parseFloat(courses[i]['credit'])
 
-			genEdArea = courses[i]['genEdArea'].substring(0,2)
+			var genEd = courses[i]['genEdArea']
+			var genEdArea = genEd.substring(0,2)
+			var genEdDept = genEd.slice(-4)
 			if(genEdArea == 'HA' && haSum < 3) {
-				haSum += 1
+				if(deptCount(genEdDept, haDepts) < 2) {
+					haSum += 1
+					haDepts.push(genEdDept)
+				}
 			} else if(genEdArea == 'SB' && sbsSum < 3) {
-				sbsSum += 1
+				if(deptCount(genEdDept, sbsDepts) < 2) {
+					sbsSum += 1
+					sbsDepts.push(genEdDept)
+				}
 			} else if(genEdArea == 'NS' && nsmSum < 3) {
-				nsmSum += 1
+				if(deptCount(genEdDept, nsmDepts) < 2) {
+					nsmSum += 1
+					nsmDepts.push(genEdDept)
+				}
 			}
 		}
 		return [credSum, nsmSum, sbsSum, haSum]
 	})
+}
+
+function deptCount(dept, array) {
+	var count = 0
+	for(e in array) {
+		if(e == dept) count += 1
+	}
+	return count
 }
