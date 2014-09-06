1e1462d1696d3c3283e008ba2abd4dfffb3bbe79
Maxwell Spiegelman <msuser@Maxs-MacBook-Pro-2.local>
2014-09-06 12:25:18 -0400

    gen ed sums

--- static/js/main.js
+++ static/js/main.js
@@ -12,10 +12,23 @@ function gradProgress(data){
 	courses = data['acadData']['classes']
 	$.getJSON('courses/'+courses.join('_'), function(courseData){
 		credSum = 0
+		nsmSum = 0
+		sbsSum = 0
+		haSum = 0
+
 		courses = courseData['courses']
 		for (var i = 0; i < courses.length; i++){
 			credSum += parseFloat(courses[i]['credit'])
+
+			genEdArea = courses[i]['genEdArea'].substring(0,2)
+			if(genEdArea == 'HA' && haSum < 3) {
+				haSum += 1
+			} else if(genEdArea == 'SB' && sbsSum < 3) {
+				sbsSum += 1
+			} else if(genEdArea == 'NS' && nsmSum < 3) {
+				nsmSum += 1
+			}
 		}
-		return credSum
+		return [credSum, nsmSum, sbsSum, haSum]
 	})
 }
